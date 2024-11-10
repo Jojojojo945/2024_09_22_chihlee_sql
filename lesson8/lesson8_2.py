69882fb768d647843a88d9550c5370450fb1b419
Jo <jo.lin811@gmail.com>
2024-11-10 14:13:13 +0800

    1110

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -2,10 +2,11 @@ import streamlit as st
 import psycopg2
 from dotenv import load_dotenv
 import os
+import pandas as pd
 load_dotenv()
 
-@st.cache_resource
-def getData():
+@st.cache_data
+def getData(country:tuple[str])->list[tuple]:
     conn = psycopg2.connect(host=os.environ['HOST'], 
                             database = os.environ['DATABASE'], 
                             user = os.environ['USER'], 
@@ -15,12 +16,13 @@ def getData():
             sql = '''
             SELECT country,市場.name,date,adj_close,volume
             FROM 股市 JOIN 市場 ON 股市.name = 市場.name
-            WHERE country = '台灣';
+            WHERE country IN %s;
             '''
-            cursor.execute(sql)
+            cursor.execute(sql,(country,))
             all_data = cursor.fetchall()
     conn.close()
     return all_data
+
 
 @st.cache_resource
 def get_country():
@@ -43,13 +45,10 @@ def user_select():
     print('使用者選擇了')
     print(st.session_state.stocks)
 
-print(getData())
-
 st.title('世界大盤分析')
-with st.sidebar:
-    default_country = '台灣'
+default_country = '台灣'
+with st.sidebar:    
     st.title('請選擇股票市場:')
-    
     st.multiselect("請選擇",get_country(),
                     default=default_country,
                     placeholder="請選擇市場",
@@ -57,4 +56,8 @@ with st.sidebar:
                     key='stocks',
                     on_change=user_select)
     st.write(default_country)
+
+df = pd.DataFrame(getData((default_country,)),columns=['國家','代號','日期','收盤價','成交量'])
+df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
+st.line_chart(data=df,x='日期',y='收盤價',color='國家')
     
